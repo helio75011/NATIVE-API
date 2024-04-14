83cd3265e1a4e03f84703445c4c27a9e9eceb755
de Breyne <111784301+helio75011@users.noreply.github.com>
2024-04-14 13:39:09 +0200

    add api

--- component/Search.js
+++ component/Search.js
@@ -1,28 +1,38 @@
-import React from 'react'
-import {View, Text, StyleSheet, Button, TextInput} from 'react-native'
-import style from '../style'
+import React from 'react';
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
+import axios from 'axios';
+import style from '../style';
 
 export default class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { r: '' };
+    }
 
-    constructor(props){
-        super(props) // constructeur parent
+    handleTextChange = (text) => {
+        this.setState({ r: text });
+    }
 
-        this.state = {
-            r : ''
+    submit = () => {
+        this.fetchData();
+    }
+
+    fetchData = async () => {
+        try {
+            const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=ec0f0883f1a23d5918a53751e8995e80&query=${this.state.r}`);
+            this.props.navigation.navigate('Result', { searchResults: response.data });
+        } catch (error) {
+            console.error(error);
         }
     }
 
-    submit(){
-        this.props.navigation.navigate('Result', {research: this.state.r})
-    }
-
-    render () {
+    render() {
         return (
             <View>
                 <Text style={style.title}>Recherche</Text>
-                <TextInput style={style.input} onChangeText={(t) => this.state.r = t} />
-                <Button title="Rechercher" onPress={ () => this.submit() } />
+                <TextInput style={style.input} onChangeText={this.handleTextChange} />
+                <Button title="Rechercher" onPress={this.submit} />
             </View>
-        )
+        );
     }
 }
